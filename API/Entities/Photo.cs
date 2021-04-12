d2a9d69f2c192b0655052777e4dbf5b126ab1a7c
Harsh Chaurasia <harshchaurasia619@gmail.com>
2021-04-12 14:42:35 +0530

    End of section 8

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("Photos")]
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string URL { get; set; }
+        public object Url { get; internal set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int AppUserId { get; set; }
+    }
+}
